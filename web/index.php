aa96ac544e08429be86688da9c8f1d06c2b6664c
Osintsevt <osintsevt@mail.ru>
2017-12-11 21:39:09 +0700

    Messages handler

--- web/index.php
+++ web/index.php
@@ -31,8 +31,15 @@ $app->post('/', function() use($app) {
 		
 		case 'message_new':
 			
+			$request_params = array(
+				'user_id' => $data->object->user_id,
+				'message' => 'Test',
+				'access_token' => getenv('VK_TOKEN'),
+				'v' => '5.69' 
 
-
+			);
+			file_get_contents('https://api.vk.com/method/messages.send?' . http_build_query($request_params));
+			return 'ok';
 
 			break;
 	}
