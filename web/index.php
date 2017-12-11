c10bb93b19281f5fa6682428b9463baca531fe87
Osintsevt <osintsevt@mail.ru>
2017-12-11 21:00:02 +0700

    Base handler

--- web/index.php
+++ web/index.php
@@ -10,16 +10,35 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
 
-// Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+  return 'Hello World!';
+});
+
+$app->post('/', function() use($app) {
+	$data = json_decode(file_get_contents("php://input"));
+
+	if (!$data) {
+		return 'ax';}
+	if ($data->secret !== getenv('VK_SECRET_TOKEN') && $data->type !== 'confirmation') {
+		return 'ax'}
+
+	switch ($data->type) {
+		case 'confirmation':
+			return getenv('VK_SECRET_TOKEN')
+			break;
+		
+		case 'message_new':
+			
+
+
+			
+			break;
+	}
+
+
+  return 'ax';
 });
 
 $app->run();
