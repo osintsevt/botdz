ab0e29b1b0f236543da64ab9e38d8ed3f16b2c80
Osintsevt <osintsevt@mail.ru>
2017-12-16 23:11:00 +0700

    IT WORKS

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
+$text = 'ДЗ пока не написали';
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
@@ -11,7 +12,6 @@ $app->get('/', function() use($app) {
 });
 $app->post('/', function() use($app) {
 	$data = json_decode(file_get_contents("php://input"));
-	$text = 'ДЗ пока не написали';
 	if (!$data)
 		return 'ax';
 	if ($data->secret !== getenv('VK_SECRET_TOKEN') && $data->type !== 'confirmation')
@@ -22,8 +22,8 @@ $app->post('/', function() use($app) {
 			break;
 		
 		case 'message_new':
-			if (preg_match('<admin>',$data->object->body)) {
-				$text = preg_replace('<admin>', '', $data->object->body);
+			if (preg_match('[admin]',$data->object->body)) {
+				$text = preg_replace('[admin]', '', $data->object->body);
 			}
 			$request_params = array(
 				'user_id' => $data->object->user_id,
