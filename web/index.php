33bfa888ebbba99379b37e22f0f532989d613a6c
Osintsevt <osintsevt@mail.ru>
2017-12-16 23:07:21 +0700

    IT MUST WORKS

--- web/index.php
+++ web/index.php
@@ -11,6 +11,7 @@ $app->get('/', function() use($app) {
 });
 $app->post('/', function() use($app) {
 	$data = json_decode(file_get_contents("php://input"));
+	$text = 'ДЗ пока не написали';
 	if (!$data)
 		return 'ax';
 	if ($data->secret !== getenv('VK_SECRET_TOKEN') && $data->type !== 'confirmation')
@@ -21,10 +22,12 @@ $app->post('/', function() use($app) {
 			break;
 		
 		case 'message_new':
-			
+			if (preg_match('<admin>',$data->object->body)) {
+				$text = preg_replace('<admin>', '', $data->object->body);
+			}
 			$request_params = array(
 				'user_id' => $data->object->user_id,
-				'message' => 'Извините! Но я еще не сертифицированный работник, да и вида на жительство не имею. А если миграционная служба пронюхает, что я без регистрации? Ну вы поняли... Должны дать документы в воскресенье, надеюсь так и будет, но не знаю<br><br>P.S Вот вам дз на завтра (только никому, контробанда в концеконцов):<br>Русский: 2 примера на каждый из четырех типов односоставных глагольных предложений, ОК, параграфы 22-27<br>Литература: басню Волк на псарне учить наизусть, песнь о Вещем Олеге<br>Биология:98 в р.т<br>История: знать всех правителей династии Романовых<br>Химия: готовимся к к/р<br>Тс... я вам ничего не говорил',
+				'message' => $text,
 				'access_token' => getenv('VK_TOKEN'),
 				'v' => '5.69' 
 			);
