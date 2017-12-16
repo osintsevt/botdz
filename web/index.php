95e47e79919cd27c7e1128a1b20caa702641083f
Osintsevt <osintsevt@mail.ru>
2017-12-17 00:11:35 +0700

    IT maybe will WORKSjhurehnu9pr]rfhyu]ог8шjhgtuoprjhueашкrjhi8

--- web/index.php
+++ web/index.php
@@ -3,7 +3,6 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 $dz = 'ДЗ пока не написали';
-putenv("DZ=$dz");
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
@@ -14,7 +13,6 @@ $app->get('/', function() use($app) {
 });
 $app->post('/', function() use($app) {
 	$data = json_decode(file_get_contents("php://input"));
-	$dz_s = preg_replace('/admin:/', '', $data->object->body);
 	if (!$data)
 		return 'ax';
 	if ($data->secret !== getenv('VK_SECRET_TOKEN') && $data->type !== 'confirmation')
@@ -25,12 +23,12 @@ $app->post('/', function() use($app) {
 			break;
 		
 		case 'message_new':
-			if (strpos($data->object->body,'admin:')) 
-				putenv("DZ=$dz_s");
+			if (strpos($data->object->body,'admin:')!==false) 
+				$GLOBALS['dz'] = preg_replace('/admin:/', '', $data->object->body);
 
 			$request_params = array(
 				'user_id' => $data->object->user_id,
-				'message' => getenv('DZ'),
+				'message' => $dz,
 				'access_token' => getenv('VK_TOKEN'),
 				'v' => '5.69' 
 			);
